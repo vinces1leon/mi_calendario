5d654c68026cdfa4bf0bdb2fc81872c402d22a21
vinces1leon <sebastian.a.vinces@gmail.com>
2025-08-22 13:00:03 -0500

    new

--- mi_calendario/settings.py
+++ mi_calendario/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,12 +75,23 @@ WSGI_APPLICATION = 'mi_calendario.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+if os.getenv("DATABASE_URL"):   # Si existe la variable (Render/producción)
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.getenv("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
-}
+else:   # Si NO existe (local), usa SQLite
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
